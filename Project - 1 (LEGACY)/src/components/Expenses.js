860b7b1a44dd434e815029ff8afb9698bcc2e69f
Shashwat0212 <shashwat0212@gmail.com>
2023-12-07 20:22:25 +0530

    6.7% of 74 hrs (63/4400) (07-12-2023)

---
+++ Project - 1 (LEGACY)/src/components/Expenses.js
@@ -0,0 +1,19 @@
+import "./ExpenseItem.css";
+import ExpenseItem from "./ExpenseItem";
+function Expenses({ expenses }) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+    </div>
+  );
+}
+export default Expenses;
